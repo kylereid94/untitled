27c5f7b1ea2f0b27bb7425e3763276b54fed15fc
kylereid94 <119461875+kylereid94@users.noreply.github.com>
2024-12-20 14:39:31 +0000

    untitled

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ['Lexend', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#472D30",
+          "secondary": "#723D46",
+          "accent": "E26D5C",
+          "neutral": "C9CBA3",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
